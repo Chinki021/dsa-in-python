34cd2482eeb08decc189db2fe56b15b393356ec1
Chinki Raj <164533712+Chinki021@users.noreply.github.com>
2026-06-18 22:22:36 +0530

    Add ReverseNumber-LeetCode.py solution
    Implement reverse integer function with sign handling.

---
+++ Basic maths/ReverseNumber-LeetCode.py
@@ -0,0 +1,25 @@
+--link : https://leetcode.com/problems/reverse-integer/
+# Approach:
+# 1. Store the sign of the number.
+# 2. Convert the number to its absolute value.
+# 3. Extract the last digit using modulo (% 10).
+# 4. Append the digit to the reversed number using:
+#       result = result * 10 + last_digit
+# 5. Remove the last digit using integer division (// 10).
+# 6. Repeat until the number becomes 0.
+# 7. Restore the original sign and return the reversed number.
+
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+class Solution:
+    def reverse(self, n):
+        sign = -1 if n<0 else 1
+        n=abs(n)
+        num=n
+        result=0
+        while num>0:
+            last_digit=num%10
+            result=result*10+last_digit
+            num=num//10
+        return sign * result
